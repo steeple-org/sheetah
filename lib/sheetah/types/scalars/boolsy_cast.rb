f0732afceacba7b375af5898e5b6b54f1555c588
Erwan Thomas <erwan.thomas@steeple.fr>
2022-06-02 12:18:49 +0200

    Boolsy casts may be strict/non-strict

--- lib/sheetah/types/scalars/boolsy_cast.rb
+++ lib/sheetah/types/scalars/boolsy_cast.rb
@@ -8,24 +8,42 @@ module Sheetah
       class BoolsyCast
         include Cast
 
-        TRUTHY = [true].freeze
-        FALSY  = [false].freeze
+        TRUTHY = [true, 1].freeze
+        FALSY  = [false, 0].freeze
         private_constant :TRUTHY, :FALSY
 
-        def initialize(truthy: TRUTHY, falsy: FALSY, **)
+        def initialize(truthy: [], falsy: [], strict: false, **)
           @truthy = truthy
-          @falsy = falsy
+          @falsy  = falsy
+          @strict = strict
         end
 
         def call(value, messenger)
+          boolsy = strict_match(value)
+          boolsy = loose_match(value) if boolsy.nil? && !@strict
+
+          return boolsy unless boolsy.nil?
+
+          messenger.error("must_be_boolsy", value: value.inspect)
+
+          throw :failure
+        end
+
+        private
+
+        def strict_match(value)
           if @truthy.include?(value)
             true
           elsif @falsy.include?(value)
             false
-          else
-            messenger.error("must_be_boolsy", value: value.inspect)
+          end
+        end
 
-            throw :failure
+        def loose_match(value)
+          if TRUTHY.include?(value)
+            true
+          elsif FALSY.include?(value)
+            false
           end
         end
       end
--- spec/sheetah/types/scalars/boolsy_cast_spec.rb
+++ spec/sheetah/types/scalars/boolsy_cast_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Sheetah::Types::Scalars::BoolsyCast do
   describe "#initialize" do
     it "setups default, conventional boolsy values" do
       expect(described_class.new).to eq(
-        described_class.new(truthy: [true], falsy: [false])
+        described_class.new(truthy: [], falsy: [], strict: false)
       )
     end
   end
@@ -33,25 +33,107 @@ RSpec.describe Sheetah::Types::Scalars::BoolsyCast do
       allow(set).to receive(:include?).with(value).and_return(bool)
     end
 
-    it "returns true when the value is truthy" do
-      stub_inclusion(truthy, value, true)
-
+    def expect_truthy(value = self.value)
       expect(cast.call(value, messenger)).to be(true)
     end
 
-    it "returns false when the value is falsy" do
-      stub_inclusion(truthy, value, false)
-      stub_inclusion(falsy, value, true)
-
+    def expect_falsy(value = self.value)
       expect(cast.call(value, messenger)).to be(false)
     end
 
-    it "adds an error message and throws :failure otherwise" do
-      stub_inclusion(truthy, value, false)
-      stub_inclusion(falsy, value, false)
-
+    def expect_failure(value = self.value)
       expect(messenger).to receive(:error).with("must_be_boolsy", value: value.inspect)
       expect { cast.call(value, messenger) }.to throw_symbol(:failure, nil)
+    end
+
+    context "when the value is truthy" do
+      before do
+        stub_inclusion(truthy, value, true)
+        stub_inclusion(falsy, value, false)
+      end
+
+      it "returns true" do
+        expect_truthy
+      end
+    end
+
+    context "when the value is falsy" do
+      before do
+        stub_inclusion(truthy, value, false)
+        stub_inclusion(falsy, value, true)
+      end
+
+      it "returns false" do
+        expect_falsy
+      end
+    end
+
+    context "when the value isn't truthy nor falsy" do
+      before do
+        stub_inclusion(truthy, value, false)
+        stub_inclusion(falsy, value, false)
+      end
+
+      it "fails with a message" do
+        expect_failure
+      end
+    end
+
+    describe "strictness" do
+      subject(:cast) { described_class.new(truthy: truthy, falsy: falsy, strict: strict) }
+
+      def stub_no_inclusions(value)
+        stub_inclusion(truthy, value, false)
+        stub_inclusion(falsy, value, false)
+      end
+
+      context "when strict" do
+        let(:strict) { true }
+
+        it "doesn't recognize `true` in spite of the user-defined truthy values" do
+          stub_no_inclusions(value = true)
+          expect_failure(value)
+        end
+
+        it "doesn't recognize `false` in spite of the user-defined falsy values" do
+          stub_no_inclusions(value = false)
+          expect_failure(value)
+        end
+
+        it "doesn't recognize `1` in spite of the user-defined truthy values" do
+          stub_no_inclusions(value = 1)
+          expect_failure(value)
+        end
+
+        it "doesn't recognize `0` in spite of the user-defined falsy values" do
+          stub_no_inclusions(value = 0)
+          expect_failure(value)
+        end
+      end
+
+      context "when non-strict" do
+        let(:strict) { false }
+
+        it "does recognize `true` in spite of the user-defined truthy values" do
+          stub_no_inclusions(value = true)
+          expect_truthy(value)
+        end
+
+        it "does recognize `false` in spite of the user-defined falsy values" do
+          stub_no_inclusions(value = false)
+          expect_falsy(value)
+        end
+
+        it "does recognize `1` in spite of the user-defined truthy values" do
+          stub_no_inclusions(value = 1)
+          expect_truthy(value)
+        end
+
+        it "does recognize `0` in spite of the user-defined falsy values" do
+          stub_no_inclusions(value = 0)
+          expect_falsy(value)
+        end
+      end
     end
   end
 end
